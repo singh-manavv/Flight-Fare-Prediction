32a1c085f338506965f2e90b8813d7e98c259e66
Manvendra Singh <MANVENDRAS2608@GMAIL.COM>
2024-02-28 16:12:59 +0530

    Removed mlflow

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,10 +13,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
 from sklearn.model_selection import train_test_split
-import mlflow
-from mlflow.models import infer_signature
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from urllib.parse import urlparse
+
 @dataclass
 class ModelTrainerConfig:
     model_file_path: str = os.path.join("artifacts", "model.pkl")
@@ -25,13 +22,7 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_train_config = ModelTrainerConfig()
-        
-    def eval_metrics(self,actual, pred):
-            rmse = np.sqrt(mean_squared_error(actual, pred))
-            mae = mean_absolute_error(actual, pred)
-            r2 = r2_score(actual, pred)
-            return rmse, mae, r2
-        
+
     def define_models_and_params(self):
         models = {
             "RandomForest": RandomForestRegressor(),
@@ -77,7 +68,7 @@ class ModelTrainer:
             },
         }
         return models, params
-    
+
     def init_model_training(self, df):
         logging.info("Initializing model training")
         try:
@@ -99,45 +90,6 @@ class ModelTrainer:
             best_model = models[best_model_name]
             save_object(self.model_train_config.model_file_path, best_model)
             logging.info("Finished model training and saved model to pickle file")
-            
-            model_names = list(params.keys())
-
-            actual_model=""
-
-            for model in model_names:
-                if best_model_name == model:
-                    actual_model = actual_model + model
-
-            best_params = params[actual_model]
-            
-            mlflow.set_registry_uri("https://dagshub.com/singh-manavv/Flight-Fare-Prediction.mlflow")
-            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-            # mlflow
-
-            with mlflow.start_run():
-
-                predicted_qualities = best_model.predict(X_test)
-
-                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
-
-                mlflow.log_params(best_params)
-
-                mlflow.log_metric("rmse", rmse)
-                mlflow.log_metric("r2", r2)
-                mlflow.log_metric("mae", mae)
-
-
-                # Model registry does not work with file store
-                if tracking_url_type_store != "file":
-
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
-                else:
-                    mlflow.sklearn.log_model(best_model, "model")
             return best_score
         except Exception as e:
             logging.error(f"Error in model training: {e}", exc_info=True)
